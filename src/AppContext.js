0bf4ce9c6806c68a8e8cfde014e4a8160117a384
naveendk <naveenkolambage@gmail.com>
2020-07-05 14:56:11 +1200

    added minor changes

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,9 +1,13 @@
-import React, {useState} from "react"
+import React, {useState,useEffect} from "react"
 const AppContext = React.createContext()
 
 function AppContextProvider(props){
     const [allPhotos,setAllPhotos] = useState([]);
-   return(
+     
+
+ 
+
+    return(
        <AppContext.Provider value={{allPhotos}}>
 
            {props.children}
