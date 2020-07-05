742ddccc978241e6ca956b0d4300676e939b2450
naveendk <naveenkolambage@gmail.com>
2020-07-06 05:38:29 +1200

    step 4 complete

--- src/AppContext.js
+++ src/AppContext.js
@@ -3,9 +3,17 @@ const AppContext = React.createContext()
 
 function AppContextProvider(props){
     const [allPhotos,setAllPhotos] = useState([]);
-     
+     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(()=>{
+        //Get the data from the api 
+        //save the data to state
+        fetch(url)
+        .then(res=>res.json())
+        .then(data => setAllPhotos(data))
 
- 
+    },[])
+  
+     console.log(allPhotos)
 
     return(
        <AppContext.Provider value={{allPhotos}}>
