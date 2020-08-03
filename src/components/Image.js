77d283c34ca6140f4f168cd2770600124dca690a
naveendk <naveenkolambage@gmail.com>
2020-08-03 21:11:09 +1200

    rftr-fvrted-added

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,18 +1,25 @@
-import React, {useState} from "react"
+import React, {useState,useContext} from "react"
+import {AppContext} from "../AppContext"
 
 
 function Image({className, img}) {
     const [ hovered,setHovered] = useState(false)
+    //const [isFavorited,setIsFavorited] = useState(false)
+    const {toggleFavorite} = useContext(AppContext)
 
-    const heartIcon =  hovered &&   <i className="ri-heart-line favorite"></i>
+    
+
+    const heartIcon =  hovered &&   <i className="ri-heart-line favorite" 
+    onClick={() => toggleFavorite(img.id)}></i>
     const cartIcon =  hovered &&   <i className="ri-add-circle-line cart"></i> 
+
     return (
         <div className={`${className} image-container`}
              onMouseEnter={()=>setHovered(true)}
              onMouseLeave={()=>setHovered(false)}
         >
             <img src={img.url} className="image-grid"/>
-              {heartIcon}
+               {heartIcon}
               {cartIcon}
         </div>
     )
